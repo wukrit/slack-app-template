4f1ad808438b59d2575814c5e616d75dd160da6b
Jeff Manian <jeffrey.manian@gmail.com>
2020-06-29 20:06:55 -0400

    basic interaction endpoint that posts back hello world

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+gem 'sinatra'
+gem 'slack-ruby-client'
+
+group :development do
+  gem 'sinatra-contrib'
+  gem 'dotenv'
+end
---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+require 'sinatra/reloader' if settings.development?
+require 'dotenv/load' if settings.development?
+require 'slack'
+
+Slack.configure do |config|
+  config.token = ENV['SLACK_BOT_TOKEN']
+end
+
+post '/interactions' do
+  payload = JSON.parse(params[:payload], symbolize_names: true)
+
+  client = Slack::Web::Client.new
+  channel_id = client.conversations_open(users: payload[:user][:id]).channel.id
+  client.chat_postMessage(channel: channel_id, text: 'Hello, world!')
+end
